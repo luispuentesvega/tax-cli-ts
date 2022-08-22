45a71d37ac764463978c4a40c249403687d97800
Luis Puentes Vega <luispuentesvega@gmail.com>
2022-08-22 13:27:28 +0700

    test(index): fix test case when all arguments are provided

--- index.test.ts
+++ index.test.ts
@@ -6,21 +6,17 @@ jest.mock('process', () => ({
   exit: mockedExit
 }));
 
-describe('index', () => {
-  test('should call exit(1) when not arguments are provided', () => {
-    index;
-    expect(mockedExit).toHaveBeenCalledWith(1);
-  });
+jest.mock('yargs', () => ({
+  argv: {
+    user: '123',
+    file: 'transaction-1line.csv',
+    type: 'CAPITOL_GAIN'
+  }
+}));
 
+describe('index', () => {
   test('should not call exit(1) when all the arguments are provided', () => {
-    jest.mock('yargs', () => ({
-      argv: {
-        user: '123',
-        file: 'transaction-1line.csv',
-        type: 'CAPITOL_GAIN'
-      }
-    }));
     index;
-    expect(mockedExit).toHaveBeenCalledWith(1);
+    expect(mockedExit).not.toHaveBeenCalled();
   });
 });
